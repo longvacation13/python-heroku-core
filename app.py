d008d99b0ab5caab68dbd5794296c0668336f180
안준영(202172) <superukie@naver.com>
2021-03-19 15:49:34 +0900

    requirements matplotlib 추가

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 # https://python-explorer.tistory.com/13
 from flask import Flask
-
 app = Flask(__name__) 
 
 import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 @app.route('/') 
 def hello_world(): 
