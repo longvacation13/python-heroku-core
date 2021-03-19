dcb51825cc8bc7a0fe0ec0dedb62d184a02667a4
안준영(202172) <superukie@naver.com>
2021-03-19 15:59:56 +0900

    #주석처리#

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ app = Flask(__name__)
 
 import numpy as np 
 import pandas as pd
+# import matplotlib.pyplot as plt
 
 @app.route('/') 
 def hello_world(): 
- df = pd.read_csv('2020년 ITEM_ID별 실적(센터)_05월.csv')
+#  df = pd.read_csv('2020년 ITEM_ID별 실적(센터)_05월.csv')
  # print(df)
- plt.figure(figsize=(10,5))
- plt.scatter(df.index, df['day_ord_cnt'])
- plt.savefig('result\graph\savefig_default.png')
+#  plt.figure(figsize=(10,5))
+#  plt.scatter(df.index, df['day_ord_cnt'])
+#  plt.savefig('result\graph\savefig_default.png')
  return "1"
  
 if __name__ == '__main__': 
