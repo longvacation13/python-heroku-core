f1ac1eb5e7a69e187abb1c259b51c241d74fcd05
안준영(202172) <superukie@naver.com>
2021-03-19 15:33:59 +0900

    plt 기능 추가(테스트)

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 # https://python-explorer.tistory.com/13
 from flask import Flask
+
 app = Flask(__name__) 
 
 import numpy as np 
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 @app.route('/') 
 def hello_world(): 
- return 'Hello Python Flask' 
+ df = pd.read_csv('2020년 ITEM_ID별 실적(센터)_05월.csv')
+ # print(df)
+ plt.figure(figsize=(10,5))
+ plt.scatter(df.index, df['day_ord_cnt'])
+ plt.savefig('result\graph\savefig_default.png')
+ return "1"
  
 if __name__ == '__main__': 
  app.run() 
