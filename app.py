00bed3d8c53c59479472da1a201948ccf47eeab6
안준영(202172) <superukie@naver.com>
2021-03-19 11:26:37 +0900

    #1 first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# https://python-explorer.tistory.com/13
+from flask import Flask
+app = Flask(__name__) 
+
+import numpy as np 
+
+
+@app.route('/') 
+def hello_world(): 
+ return 'Hello World!' 
+ 
+if __name__ == '__main__': 
+ app.run() 
+ 
