23cffefb6db55fcce493b1a1521e405fceabda46
안준영(202172) <superukie@naver.com>
2021-03-22 15:22:26 +0900

    secure key 값 추가

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("config\heroku-pro-firebase-adminsdk-na1xm-9e49a017fd.json")
+firebase_admin.initialize_app(cred)
+
 
 @app.route('/') 
 def hello_world(): 
